1b989621b65417ad0b0439efd314605e89becf0a
Yohan Silva <yohanps.dev@gmail.com>
2022-01-06 18:58:59 -0300

    Project created

---
+++ src/main/java/project/employeeapi/employeeapi/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package project.employeeapi.employeeapi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/employee")
+public class EmployeeController {
+
+    @GetMapping
+    public String getBook(){
+        return "API test!";
+    }
+}
